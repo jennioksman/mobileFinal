c8f5ee7ff7598479c99e71cd34ef485b84171fc1
jennioksman <c3okje00@students.oamk.fi>
2025-02-10 12:34:29 +0200

    Core of the front page made

--- components/components.js
+++ components/components.js
@@ -1,8 +1,29 @@
-import { Text } from "react-native-paper"
+import { useState } from "react";
+import { View, TouchableOpacity } from "react-native"
+import { Text, Card } from "react-native-paper"
+
 
 export function Home(){
+
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
     return(
-      <Text variant='headlineMedium'>Home</Text>
+      <View>
+        <Text variant='headlineMedium'>Welcome</Text>
+        <Card>
+          <Card.Cover source={require('../assets/crossfit.jpg')} />
+          <TouchableOpacity onPress={onPress}>
+            <Text variant="headlineSmall">Add New Workout</Text>
+        </TouchableOpacity>
+        <Card>
+          <Card.Cover source={require('../assets/run.jpg')} />
+          <TouchableOpacity onPress={onPress}>
+            <Text variant="headlineSmall">Show My Workouts</Text>
+        </TouchableOpacity>
+        </Card>
+        </Card>
+      </View>
     )
   }
   
